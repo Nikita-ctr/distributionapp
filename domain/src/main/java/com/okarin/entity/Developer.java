6805957540c7eab0902a944e4518f91d5fa07cd7
Nikita <xmil.nik@gmail.com>
2021-12-05 17:05:26 +0300

    domain

--- domain/src/main/java/com/okarin/entity/Developer.java
+++ domain/src/main/java/com/okarin/entity/Developer.java
@@ -1,4 +1,43 @@
 package com.okarin.entity;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "developers")
 public class Developer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME", length = 30)
+    private String first_name;
+
+    @Column(name = "LAST_NAME",length = 90)
+    private String last_name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    @org.hibernate.annotations.CreationTimestamp
+    private Date dateOfBirth;
+
+    @NotNull
+    @Column(name = "PROJECT_HOURS", length = 60)
+    private int projectHours;
+
+    @OneToMany
+    private Set<Project> projects = new HashSet<>();
+
+    public Developer() {
+    }
 }
--- domain/src/main/java/com/okarin/entity/Project.java
+++ domain/src/main/java/com/okarin/entity/Project.java
@@ -1,4 +1,26 @@
 package com.okarin.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private Developer developers;
+
+    public Project() {
+    }
 }
