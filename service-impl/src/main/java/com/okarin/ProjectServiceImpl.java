7796ca8d3e5b9ab792bf6c6c06fabff8608d5cec
Nikita-ctr <xmil.nik@gmail.com>
2022-01-06 01:44:05 +0300

    rest controller test

--- service-impl/src/main/java/com/okarin/ProjectServiceImpl.java
+++ service-impl/src/main/java/com/okarin/ProjectServiceImpl.java
@@ -3,6 +3,7 @@ package com.okarin;
 import com.okarin.entity.Project;
 import com.okarin.exceptions.ProjectNotFoundExeption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
--- service/src/main/java/com/okarin/ProjectService.java
+++ service/src/main/java/com/okarin/ProjectService.java
@@ -5,9 +5,11 @@ import com.okarin.entity.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public interface ProjectService {
 
